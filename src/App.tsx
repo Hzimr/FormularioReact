fa0a39a4adf72090b1c5c1495db333b7d01d41be
Hítalo Rodrigues <hitalo005@gmail.com>
2023-07-31 15:30:37 -0300

    feat: create User in form

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
+import { useState } from 'react'
 import './styles/global.css'
+import { useForm } from 'react-hook-form'
 
 export function App() {
+  const [output, setOutput] = useState('')
+  const { register, handleSubmit } = useForm()
   
+  function createUser(data: any) {
+    setOutput(JSON.stringify(data, null, 2)) 
+  }
+
+  // High-order function (programação funcional).
+  // função que envia uma função e recebe uma função.
+  // .map, .reduce, .find.
+
   return (
-    <main className="h-screen bg-zinc-50 flex items-center justify-center">
-      <form className='flex flex-col gap-4 w-full max-w-xs'>
+    <main className="h-screen bg-zinc-950 text-zinc-300 flex flex-col gap-10 items-center justify-center">
+      <form 
+        onSubmit={handleSubmit(createUser)}
+        className='flex flex-col gap-4 w-full max-w-xs'>
         <div className="flex flex-col gap-1">
           <label htmlFor="">E-mail</label>
           <input 
             type="email" 
-            name="email"
-            className='border border-zinc-200 shadow-sm rounded h-10' />
+            className='border border-zinc-600 shadow-sm rounded h-10 px-3 bg-zinc-800 text-white'
+            {...register('email')}
+          />
         </div>
         <div className="flex flex-col gap-1">
           <label htmlFor="">Senha</label>
           <input 
             type="password" 
-            name='password'
-            className='border border-zinc-200 shadow-sm rounded h-10'/>
+            className='border border-zinc-600 shadow-sm rounded h-10 px-3 bg-zinc-800 text-white'
+            {...register('password')}
+          />
         </div>
 
         <button 
@@ -25,6 +41,7 @@ export function App() {
           className='bg-emerald-500 rounded font-semibold text-white h-10 hover:bg-emerald-600'
         >Salvar</button>
       </form>
+      <pre>{output}</pre>
     </main>
   )
 }
